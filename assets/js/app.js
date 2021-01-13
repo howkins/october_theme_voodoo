ee8a4f29105b6d10dbefe39b8177699a6f2e6080
root <root@project-dev.pensoft.local>
2021-01-13 17:06:47 +0200

    fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,10 +13,24 @@ var keepFooter = function(documentHasScroll){
     }
 }
 
+
+
 $(document).ready(function() {
 	var loggedInMenuNavbar = $('.navbar-loggedin-user');
 	loggedInMenuNavbar.find('#menu').removeAttr('id');
+
+	$('body').on('click', '.accordion-toggle', function () {
+		if ($(this).next(".accordion-content").is(':visible')) {
+			$(this).next(".accordion-content").slideUp(300);
+			$(this).children(".plusminus").html('<span class="plus"></span>');
+		} else {
+			$(this).next(".accordion-content").slideDown(300);
+			$(this).children(".plusminus").html('<span class="minus"></span>');
+		}
+	});
 });
+
+
 
 function encodeURIObject(data){
     return Object.keys(data).map(function (i) {
